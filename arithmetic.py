a50869d7046088e53a259963ad16864c5182503c
bipin <bipinhyoju@gmail.com>
2025-05-05 13:39:33 +0545

    first commit

---
+++ arithmetic.py
@@ -0,0 +1,21 @@
+def addition(a, b):
+    "Returns the sum of two numbers"
+    return a + b
+
+def multiply(a, b):
+    "Returns the product of two numbers"
+    return a * b
+
+def division(a, b):
+    "Returns the division operation between the dividend and divisor"
+    return a / b
+
+def factorial(n):
+    "Returns the factorial of n"
+    i = 0
+    result = 1
+    while(i != n):
+        i = i + 1
+        result = result * i
+    return result
+
---
+++ arithmetic1.py
@@ -0,0 +1,13 @@
+import arithmetic
+
+result1 = arithmetic.addition(2, 3)
+print(result1)
+
+result2 = arithmetic.multiply(3, 5)
+print(result2)
+
+result3 = arithmetic.division(10, 4)
+print(result3)
+
+result4 = arithmetic.factorial(5)
+print(result4)
